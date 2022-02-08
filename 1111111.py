1a9d18ecb5cab90dbd937da3d604bf968b6248da
AdilKulzhibayev <98277080+AdilKulzhibayev@users.noreply.github.com>
2022-02-09 00:31:56 +0600

    Update 1111111.py

--- 1111111.py
+++ 1111111.py
@@ -1,1 +1,13 @@
-#aaaaaaaaa
+from sys import argv
+
+print('Hey! Вы написали сколько часов проработал ваш сотрудник, сколько он получает в час и какую премию вы собираетесь ему заплатить!! Мы расчитали все!')
+
+def sallary_count(hours,rate, bonus):
+    return hours * rate + bonus
+print(argv)
+
+print(sallary_count(int(argv[1]),int(argv[2]),int(argv[3])))
+
+
+
+
